694f0fd209d94d18f916c2f65a8414877507171b
Willian Scaquett <willian.scaquett@gmail.com>
2023-08-17 20:30:16 -0300

    Init

---
+++ src/main/java/com/fightdevs/FightForge/FightForgeApplication.java
@@ -0,0 +1,13 @@
+package com.fightdevs.FightForge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FightForgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fightdevs/FightForge/FightForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fightdevs.FightForge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FightForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
